50293d2642293c8cc1d4e7365b4a3c7819fa98ce
MichaelChen <o0ochenjundao0o@gmail.com>
2022-11-01 16:47:03 +0700

    First upload

---
+++ 03-Terraform-Settings-Providers-Resources/c2-ec2instance.tf
@@ -0,0 +1,9 @@
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami           = "ami-094bbd9e922dc515d"
+  instance_type = "t3.micro"
+  user_data     = file("${path.module}/app1-install.sh") # Remember!
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
