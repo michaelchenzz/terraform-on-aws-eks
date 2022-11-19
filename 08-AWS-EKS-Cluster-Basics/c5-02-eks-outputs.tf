4578bd99a106fd025c9a5c26151193b834ada2e3
MichaelChen <o0ochenjundao0o@gmail.com>
2022-11-19 22:28:42 +0700

    08-AWS-EKS-Cluster-Basics clean up

--- 08-AWS-EKS-Cluster-Basics/c5-02-eks-outputs.tf
+++ 08-AWS-EKS-Cluster-Basics/c5-02-eks-outputs.tf
@@ -66,6 +66,7 @@ output "node_group_public_version" {
 }
 
 # EKS Node Group Outputs - Private
+/*
 output "node_group_private_id" {
   description = "Node Group 1 ID"
   value       = aws_eks_node_group.eks_ng_private.id
@@ -85,3 +86,4 @@ output "node_group_private_version" {
   description = "Private Node Group Kubernetes Version"
   value       = aws_eks_node_group.eks_ng_private.version
 }
+*/
--- 08-AWS-EKS-Cluster-Basics/c5-08-eks-node-group-private.tf
+++ 08-AWS-EKS-Cluster-Basics/c5-08-eks-node-group-private.tf
@@ -1,4 +1,5 @@
 # Create AWS EKS Node Group - Private
+/*
 resource "aws_eks_node_group" "eks_ng_private" {
   cluster_name = aws_eks_cluster.eks_cluster.name
 
@@ -40,3 +41,4 @@ resource "aws_eks_node_group" "eks_ng_private" {
     Name = "Private-Node-Group"
   }
 }
+*/
